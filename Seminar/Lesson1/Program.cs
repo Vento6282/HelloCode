6a6bc3d74b68f56d043d854b575ee181afe4cb4e
AAndreev <koriolanrus@gmail.com>
2023-09-18 20:54:30 +0300

    Seminar lesson1 Example

--- Seminar/Lesson1/Program.cs
+++ Seminar/Lesson1/Program.cs
@@ -1,5 +1,37 @@
 ﻿// Example 1
+// Console.Clear();
+// Console.WriteLine("Введите число:");
+// int numberA = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Квадрат числа " + numberA + " = " + numberA * numberA);
 
-Console.WriteLine("Введите число:");
-int numberA = Convert.ToInt32(Console.ReadLine());
-Console.Write("Квадрат числа " + numberA + " = " + numberA * numberA);
+// Example 2
+// Console.Clear();
+// Console.WriteLine("Введите первое число:");
+// int numberA = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите второе число:");
+// int numberB = Convert.ToInt32(Console.ReadLine());
+// if (numberA * numberA == numberB)
+//     Console.WriteLine(numberB + " является квадратом числа " + numberA);
+// else
+//     Console.WriteLine(numberB + " НЕ является квадратом числа " + numberA);
+
+// Example 3
+ Console.Clear();
+ Console.WriteLine("Введите номер дня недели:");
+ int numberA = Convert.ToInt32(Console.ReadLine());
+ if (numberA == 1)
+    Console.WriteLine("Понедельник");
+ else if (numberA == 2)
+    Console.WriteLine("Вторник");
+ else if (numberA == 3)
+    Console.WriteLine("Среда");
+ else if (numberA == 4)
+    Console.WriteLine("Четверг");
+ else if (numberA == 5)
+    Console.WriteLine("Пятница");
+ else if (numberA == 6)
+    Console.WriteLine("Суббота");
+ else if (numberA == 7)
+    Console.WriteLine("Воскресение");
+else 
+    Console.WriteLine("Дня недели с таким номером нет");
