b2c72c5ea57f3ebb65a43129d939ed833e820248
AAndreev <koriolanrus@gmail.com>
2023-09-24 20:21:18 +0300

    Add Example001S update

--- Example001S/Program.cs
+++ Example001S/Program.cs
@@ -1,1 +1,58 @@
-﻿
+﻿Console.Clear();
+// int[] array = new int [8];
+// for(int i = 0; i < array.Length; i++)
+//     array[i] = new Random().Next(-100, 101);
+// for(int i = 0; i < array.Length; i++)
+//     Console.Write(array[i] + " ");
+// Console.WriteLine();
+// Console.WriteLine(string.Join(" ", array));
+
+
+// string name = "Alexandr";
+// int age = 30;
+// string city = "Moscow";
+// Console.WriteLine($"Меня зовут {name}. Мне {age} и я живу в городе {city}");
+
+
+// int number = new Random().Next(10,99);
+// int num1 = number/10;
+// int num2 = number % 10;
+// Console.WriteLine(number);
+// if (num1 > num2) 
+//     Console.WriteLine(num1);
+// else
+//     Console.WriteLine(num2);
+
+
+// int number = new Random().Next(100,1000);
+// int num1 = number/100*10;
+// int num2 = number % 10;
+// int num3 = num1 + num2;
+// Console.WriteLine(number + " " + num1 + " " + num2 + " " + num3 + " ");
+
+
+// int num = Convert.ToInt32(Console.ReadLine());
+// if (num % 7 == 0 && num % 8 == 0)
+//     Console.WriteLine("Yes");
+// else
+//     Console.WriteLine("No");
+
+
+// Console.WriteLine("Введите первое число:");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите второе число:");
+// int num2 = Convert.ToInt32(Console.ReadLine());
+// if (num1 % num2 == 0)   
+//     Console.WriteLine($"{num1} кратно {num2}");
+// else    
+//     Console.WriteLine($"{num1} не кратно {num2} .Остаток от деления = {num1 % num2}");
+
+
+// Console.WriteLine("Введите первое число:");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите второе число:");
+// int num2 = Convert.ToInt32(Console.ReadLine());
+// if (num1 * num1 == num2 || num2 * num2 == num1)
+//     Console.WriteLine("Yes");
+// else
+//     Console.WriteLine("No");
